c365d5ab6a6b91e8ac54af6cb89e39d3dc93777d
megafabr <megafabr@yandex.ru>
2019-10-25 14:59:19 +0300

    constant IDENTICALS + .normalize_letter + normaslized_letters

--- lib/game.rb
+++ lib/game.rb
@@ -1,55 +1,80 @@
 class Game
-TOTAL_ERRORS_ALLOWED = 7
+  TOTAL_ERRORS_ALLOWED = 7
+  IDENTICALS = {
+      "Й" => "И",
+      "Ё" => "Е",
+    }
 
   def initialize(word)
     @letters = word.chars
     @user_guesses = []
-  end
-
-  def errors_allowed
-  TOTAL_ERRORS_ALLOWED - errors_made
-  end
-
-  def errors_made
-  return errors.length
+    @normalized_letters = []
+    @normalized_letters = normalized_letters
   end
 
   def errors
-    return @user_guesses - @letters
+    @user_guesses - @normalized_letters
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
   end
 
   def letters_to_guess
-    result =
-      @letters.map do |letter|
-        if @user_guesses.include?(letter)
-          letter
-        else
-          nil
-        end
+    @letters.map do |letter|
+      if @user_guesses.include?(normalize_letter(letter))
+        letter
+      else
+        nil
       end
-      return result
+    end
   end
 
   def lost?
-    return errors_allowed == 0
+    errors_allowed == 0
   end
 
   def over?
-    return won? || lost?
+    won? || lost?
   end
 
   def play!(letter)
+    if letter == "Й" || letter == "Ё"
+      letter = normalize_letter(letter)
+    end
     if !over? && !@user_guesses.include?(letter)
-        # ничего не делает
-        @user_guesses << letter
+      @user_guesses << letter
     end
   end
 
   def won?
-    return (@letters - @user_guesses).empty?
+    (@normalized_letters - @user_guesses).empty?
   end
 
   def word
-    return @letters.join
+    @letters.join
+  end
+
+  def normalize_letter(letter)
+    IDENTICALS[letter] || letter
+  end
+
+  #def normalized_letters
+   # @letters.each do |letter|
+    #  if letter == "Й" || letter == "Ё"
+     #   @normalized_letters << normalize_letter(letter)
+      #else
+       # @normalized_letters << letter
+      #end
+    #end
+     # @normalized_letters
+  #end
+
+  def normalized_letters
+    @letters.map{ |letter| normalize_letter(letter) }
   end
 end
